01d517b8397093b79932268893942cc9129e168d
lkk <dsadas1@gmail.com>
2017-02-24 16:53:12 +0800

    V 1.0

--- icon_spiders.py
+++ icon_spiders.py
@@ -14,13 +14,13 @@ from bs4 import BeautifulSoup
 def icon_spiders(url,num):
     #主程序
     r=requests.get(url)
-    if r.status_code==200:
-        r.encoding="utf8"
-        soup=BeautifulSoup(r.text,"lxml")
-        img=soup.find_all("img",{"class":"lazy"})
+    if r.status_code == 200:
+        r.encoding = "utf8"
+        soup = BeautifulSoup(r.text,"lxml")
+        img = soup.find_all("img",{"class":"lazy"})
         for i in img:
-            r=requests.get(i["src"])
-            if r.status_code==200:
+            r = requests.get(i["src"])
+            if r.status_code == 200:
                 with open("img/"+str(num)+".jpg","wb") as f:
                     f.write(r.content)
                     time.sleep(0.5)
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     num = 0
     for i in range(2,1000):
         try:
-            url="http://www.woyaogexing.com/touxiang/index_%d.html"%i
-            num=icon_spiders(url,num)
+            url = "http://www.woyaogexing.com/touxiang/index_%d.html"%i
+            num = icon_spiders(url,num)
         except:
             num += 1
             continue
